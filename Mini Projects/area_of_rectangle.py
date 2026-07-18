85471bd941d475fa6d2dfd77adf4a59764a81fe3
DevDivyanshuV <devvarshney54@gmial.com>
2026-07-19 05:06:38 +0530

    Area of Rectangle

---
+++ Mini Projects/area_of_rectangle.py
@@ -0,0 +1,7 @@
+# input (area of rectangle)
+length=float(input("Enter length of Rectangle : "))
+width=float(input("Enter width of Rectangle : "))
+area=length*width
+print(f"area of rectangle is: {area} cm²")    
+
+# for (^2) numlock on + alt + 0178
