741c4b470398b3c6b08414d518054b5c8533fec9
DevDivyanshuV <devvarshney54@gmial.com>
2026-07-19 05:20:44 +0530

    Calculator using operators and elif

---
+++ Mini Projects/Calculator.py
@@ -0,0 +1,21 @@
+# calculator
+
+operators=input("Enter the operator (+ - / * **): ")
+numc1=float(input("Enter the first number: "))
+numc2=float(input("Enter the second number: "))
+
+if operators =="+":
+    result=numc1+numc2
+elif operators =="-":
+    result=numc1-numc2
+elif operators =="/":
+    result=numc1/numc2
+elif operators =="*":
+    result=numc1*numc2
+elif operators =="**":
+    result=numc1**numc2
+elif operators not in ["+","-","/","*","**"]:
+    result=f"Check the operator again. '{operators}' is not valid"
+
+
+print(f"result is = {result}")
