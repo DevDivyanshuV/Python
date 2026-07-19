e050eac20093d007da1847269b4041b200945609
DivyanshuVarshneyGit <devvarshney54@gmial.com>
2026-07-19 22:39:07 +0530

    input and assignment basic

--- Python-Mini-Projects/1_Area_of_rectangle.py
+++ Python-Mini-Projects/1_Area_of_rectangle.py
@@ -6,6 +6,4 @@
 length=float(input("Enter length of Rectangle : "))
 width=float(input("Enter width of Rectangle : "))
 area=length*width
-print(f"area of rectangle is: {area} cm²")    
-
-# for (^2) numlock on + alt + 0178
+print(f"area of rectangle is: {area} cm²")    # for (^2) numlock on + alt + 0178
