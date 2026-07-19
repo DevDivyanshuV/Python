31c4671ac45514435b813e22d807ce0dfa17363e
DivyanshuVarshneyGit <devvarshney54@gmial.com>
2026-07-19 17:42:20 +0530

    Used assignment and Elif function

--- Python-Mini-Projects/4_Calculator_&_Bill_Calculator.py
+++ Python-Mini-Projects/4_Calculator_&_Bill_Calculator.py
@@ -1,6 +1,5 @@
-# calculator
     # Use of assignments and elif function
-
+# calculator
 
 operators=input("Enter the operator (+ - / * **): ")
 numc1=float(input("Enter the first number: "))
@@ -21,3 +20,28 @@ elif operators not in ["+","-","/","*","**"]:
 
 
 print(f"result is = {result}")
+
+# Bill Calculator
+
+amount=float(input("Enter the shopping amount: "))
+discount=0
+delivery=50
+
+if amount<0:
+    print("invalid value")
+elif amount<1000:
+    pass
+elif amount<5000:
+    discount=0.10
+    
+elif amount<10000:
+    discount=0.20
+   
+elif 10000<=amount:
+    discount=0.30
+    delivery=0
+else:
+    print("Invalid input")
+
+bill=amount*(1-discount)+delivery
+print(f"Thanks for shopping\nYour bill amount is Rs. {round(bill,2)} after {discount*100}% discount.")
